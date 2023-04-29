34acff9f36a25051b693861b629b95edec684ba6
AllaKaplia <118439299+AllaKaplia@users.noreply.github.com>
2023-04-30 01:17:51 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -4,9 +4,8 @@ function fetchCountries(name) {
     return fetch(`${URL_BASE}/name/${name}?fields=name,capital,population,flags,languages`)
     .then(response => {
         if(!response.ok){
-            throw new Error(response.statusText || response.status.toString())
+            throw new Error(response.status)
         }
-
         return response.json();
     })
 }
