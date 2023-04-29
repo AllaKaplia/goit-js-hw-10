6fa022056c37a18940c8f8be21006f7faffccb99
AllaKaplia <118439299+AllaKaplia@users.noreply.github.com>
2023-04-30 01:17:56 +0300

    Update error.js

--- src/js/error.js
+++ src/js/error.js
@@ -1,5 +1,7 @@
+import Notiflix from 'notiflix';
+
 function onFetchError(error) {
-    if(error.status === 404){
+    if(error.message === '404'){
         Notiflix.Notify.failure('Oops, such a country name does not exist');
     }
     Notiflix.Notify.warning('Enter the correct country name!');
